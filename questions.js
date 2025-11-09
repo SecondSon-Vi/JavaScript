d7d717939eeadecc0aa144cfd015e1354cf3b49c
SecondSon-Vi <cooldismokerowe@gmail.com>
2025-11-09 12:27:11 +0300

    Test example 01

---
+++ questions.js
@@ -0,0 +1,17 @@
+
+
+//questionsAndAnswers
+
+const questions = [
+    {
+        question: "Что такое JavaScript?",
+        options: ["Язык разметки", "Язык программирования", "Браузер", "Процессор"],
+        answer:  "Язык программирования"
+    },
+
+    {
+        question: "2+2 = ?",
+        options: ["4", "22", "3", "7"],
+        answer: "4"
+    },
+]
--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-alert('Привет из внешнего файла!');
+
+//alert('Привет из внешнего файла!');
+
+
+
+//question.innerText = questions[current].question;
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//  for (let i = 0, i < questions,length, i++) {
+        let response = window.prompt()
+    }
+*/
